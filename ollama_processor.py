54a6988da08dd635b89776390793f25d9dc41470
LewiysArito <nikita.kasimov.1044@gmail.com>
2025-10-08 23:08:29 +0300

    feat: generate prompt from template

--- ollama_processor.py
+++ ollama_processor.py
@@ -1,22 +1,33 @@
 import abc
+from typing import List
 from ollama import Client
-from config import OLLAMA_MODEL, OLLAMA_TEMPERATURE, OLLAMA_CLIENT
+from config import OLLAMA_MODEL, OLLAMA_TEMPERATURE, OLLAMA_CLIENT, OLLAMA_TEMPLATE_TEXT 
 
 class AbstractLLMClient(abc.ABC):
     @abc.abstractmethod
-
     def generate(self, 
-        prompt: str,
-        model: str,
-        temperature: float = 0.7,
+        prompt: str, 
+        model: str = OLLAMA_MODEL, 
+        temperature: float = OLLAMA_TEMPERATURE
     )-> str:
+        """Generate response from llm"""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def generate_prompt_from_template(self,
+        query: str,
+        sources: List[str],
+        template: str = OLLAMA_TEMPLATE_TEXT
+    ):
+        """Generate prompt from template"""
         raise NotImplementedError
     
 class OllamaProcessor(AbstractLLMClient):
     def __init__(self, client = OLLAMA_CLIENT):
         self.client = client
 
-    def generate(self, prompt: str, model: str = OLLAMA_MODEL, temperature: float = OLLAMA_TEMPERATURE )-> str:
+    def generate(self, prompt: str, model: str = OLLAMA_MODEL, temperature: float = OLLAMA_TEMPERATURE)-> str:
+        """Generate response from llm"""
         try:
             response = self.client.generate(
                 model=model,
@@ -29,3 +40,14 @@ class OllamaProcessor(AbstractLLMClient):
             return response["response"]
         except Exception as e:
             raise Exception(f"Failed to generate completion: {e}") from e
+    
+    def generate_prompt_from_template(self,
+        query: str,
+        sources: List[str],
+        template: str = OLLAMA_TEMPLATE_TEXT
+    ):
+        """Generate prompt from template"""
+        prompt = template.replace("{query}", query)
+        prompt = prompt.replace("{sources}", sources)
+
+        return self.generate(prompt)
