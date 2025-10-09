eb78315d32b85995a803b9936b5f63be07477355
LewiysArito <nikita.kasimov.1044@gmail.com>
2025-10-09 23:44:59 +0300

    feat: added feature delete, show, clear, search rag

--- domain.py
+++ domain.py
@@ -1,38 +1,57 @@
 import os
-from typing import List
+from typing import List, Optional
 
-from ollama_processor import OllamaProcessor
-from qdrant_repository import CustomPage, QdrantRepository, CustomChunk
+from ollama_processor import OllamaProcessor, AbstractLLMClient
+from qdrant_repository import CustomPage, QdrantRepository, CustomChunk, AbstractVectorDB
 from config import get_document_dir
-from document_processor import PDFProcessor
+from document_processor import PDFProcessor, AbstractDocumentProcessor
 
-qdrant_rep = QdrantRepository()
-pdf_processor = PDFProcessor()
-ollama_processor = OllamaProcessor()   
-document_dir = get_document_dir()
+DOCUMENT_DIR = get_document_dir()
 
 class DocumentVector:
     """
     Class for managing document vectors in a vector database
     """
-    def __init__(self, 
-            qdrant_rep = qdrant_rep,
-            pdf_processor = pdf_processor,
-            ollama_processor = ollama_processor
-        ):
-        self.qdrant_rep =  qdrant_rep
-        self.pdf_processor = pdf_processor
-        self.ollama_processor = ollama_processor
-        self.filedir = os.path.join(os.getcwd(), )
+    CHUNK_COLLECTION_ENDING = "_chunks" 
+    PAGE_COLLECTION_ENDING = "_pages"
 
-    def _get_collection_basename(filename: str):
+    def __init__(self, 
+            vector_rep: Optional[AbstractVectorDB] = None,
+            pdf_processor: Optional[AbstractDocumentProcessor] = None,
+            llm_processor: Optional[AbstractLLMClient] = None,
+            document_dir = DOCUMENT_DIR
+        ):
+
+        if not vector_rep:
+            vector_rep = QdrantRepository()
+        
+        if not pdf_processor:
+            pdf_processor = PDFProcessor()
+        
+        if not llm_processor:
+            llm_processor = OllamaProcessor()
+        
+        self.vector_rep = vector_rep
+        self.pdf_processor = pdf_processor
+        self.llm_processor = llm_processor
+
+        self.filedir = os.path.join(os.getcwd(), document_dir)
+                
+    def _get_collection_basename(self, filename: str):
+        """
+        Transforms filename to basename
+        """
         return filename.split(".")[0]
 
-    def _get_chunks_collection_name(basename: str):
-        return basename + "_chunks"
+    def _get_chunks_collection_name(self, basename: str):
+        """
+        Transforms basename to collection chunks
+        """
+        return basename + self.CHUNK_COLLECTION_ENDING
     
-    def _get_pages_collection_name(basename: str):
-        return basename + "_pages"
+    def _get_pages_collection_name(self, basename: str):
+        """Transforms basename to collection pages"""
+        return basename + self.PAGE_COLLECTION_ENDING
 
     def upload_file(self, filename: str):
         """
@@ -46,7 +65,7 @@ class DocumentVector:
         col_pages_name = self._get_pages_collection_name(basename)
         col_chunks_name = self._get_chunks_collection_name(basename)
         
-        documents = pdf_processor.load_and_split_file(
+        documents = self.pdf_processor.load_and_split_file(
             filename
         )
 
@@ -54,50 +73,104 @@ class DocumentVector:
             for document_page in documents["pages"]
         ]
 
-        chunks = [CustomChunk(document_page.metadata.get("page", 0), document_page.page_content, col_pages_name) 
-            for document_page in documents["chunks"]
+        chunks = [CustomChunk(document_chunk.metadata.get("page", 0), document_chunk.page_content, col_pages_name) 
+            for document_chunk in documents["chunks"]
         ]
 
-        qdrant_rep.upload_pages(
+        self.vector_rep.upload_pages(
             collection=col_pages_name,
             pages=pages,
             source=filename
         )
 
-        qdrant_rep.upload_chunks(
+        self.vector_rep.upload_chunks(
             collection=col_chunks_name,
             chunks=chunks,
             source=filename
         )
 
-    def search_from_file(self, filename: str, prompt: str):
+    def find_chunks_from_file(self, filename: str, query: str)->List[str]:
         """
-        Search 
+        Search for relevant text chunks in the specified file.
         """
 
         basename = self._get_collection_basename(filename)
         col_chunks_name = self._get_chunks_collection_name(basename)
 
-        values = qdrant_rep.search(col_chunks_name, prompt)
-        
-        return values
+        values = self.vector_rep.search(col_chunks_name, query) 
+        return [value["text"] for value in values]
+    
+    def rag_search_from_file(self, filename: str, query: str)->str:
+        """
+        Searches for relevant pages in the file and also generates a template, which is sent to LLM for processing with the expectation of the result
+        """
+        basename = self._get_collection_basename(filename)
+        col_chunks_name = self._get_chunks_collection_name(basename)
+        col_pages_name = self._get_pages_collection_name(basename)
 
-    def delete_file(self, filename: str):
+        pages = self.vector_rep.get_relevant_documents(col_chunks_name, query)
+
+        texts = []
+        for page in pages:
+            texts.append(self.vector_rep.get_chunk_by_page(page, col_pages_name))
+
+        result = self.llm_processor.generate_prompt_from_template(
+            query,
+            "\n\n".join(texts),
+        )
+
+        return result
+
+    def delete_rag_file(self, filename: str):
         """
-        Удаляет файл и всю информацию из векторной базы данных
+        Delete collection and file by filename
         """
+        file_path = os.path.join(self.filedir, filename)  
+        
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"File {filename} not found")
         
         basename = self._get_collection_basename(filename)
         col_pages_name = self._get_pages_collection_name(basename)
         col_chunks_name = self._get_chunks_collection_name(basename)
 
-    def show_all_files(self, filename: str):
+        self.vector_rep.delete_collection(
+            collection_name=col_chunks_name, 
+        )
+
+        self.vector_rep.delete_collection(
+            collection_name=col_pages_name, 
+        )
+
+        os.remove(file_path)
+
+    def clear_all(self):
         """
-        показывает
+        Deleted from vector database all documents, loaded for rag
         """
-        pass
+        collections = list(filter(
+            lambda value:
+            value.endswith(self.CHUNK_COLLECTION_ENDING) or 
+            value.endswith(self.PAGE_COLLECTION_ENDING),
+            self.vector_rep.get_all_collections()
+        ))
+
+        self.vector_rep.delete_collections(collections)
+
+        for filename in os.listdir(self.filedir):
+            file_path = os.path.join(self.filedir, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
 
     def show_all_collections(self)->List[str]:
-        """Show All"""
-        return self.qdrant_rep.get_all_collections()
+        """Show all collections for rag"""
+
+        return list(filter(
+            lambda value:
+            value.endswith(self.CHUNK_COLLECTION_ENDING) or 
+            value.endswith(self.PAGE_COLLECTION_ENDING),
+            self.vector_rep.get_all_collections()
+        ))
+    
+    
     
